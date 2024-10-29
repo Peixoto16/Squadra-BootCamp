5848f36604ce15f3f00c24cdd8782748213e6287
Israel Peixoto <israel1peixoto@gmail.com>
2024-10-29 01:01:42 -0300

    Config Projeto

---
+++ src/main/java/israel/squadra_bootcamp/SquadraBootcampApplication.java
@@ -0,0 +1,13 @@
+package israel.squadra_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadraBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadraBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/israel/squadra_bootcamp/SquadraBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package israel.squadra_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadraBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
